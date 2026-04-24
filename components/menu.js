3ce369ce771607be9e824c4ca626c18e39d7e542
Cauã Gustavo <caua.g.silva16@aluno.senai.br>
2026-04-24 07:43:35 -0300

    Inicial

---
+++ components/menu.js
@@ -0,0 +1,36 @@
+export const iniciarMenu = () =>{
+    const menus= document.querySelectorAll('[data-menu]');
+    
+    console.log(menus)
+
+    menus.forEach((menu)=>{
+        const botao = menu.querySelector('.menu__botao')
+
+        const toggleMenu = () =>{
+            menu.classList.toggle('active');
+
+            const ativo = menu.classList.contains('active');
+            botao.setAttribute('aria-expanded', ativo)
+            botao.setAttribute('aria-label', ativo ? 'Fechar Menu' : 'Abrir Menu')
+        };
+
+        const fecharComEsc = (event) =>{
+            const isEsc = event.key === 'Escape';
+            const ativo = menu.classList.contains('active');
+
+            if (isEsc && ativo) {
+                menu.classList.remove('active');
+                botao.setAttribute('aria-expanded',false);
+                botao.setAttribute('aria-label','Abrir Menu');
+
+                botao.focus();
+            };
+        };
+
+        botao.addEventListener('pointerdown',toggleMenu);
+        document.addEventListener('keydown',fecharComEsc)
+
+    });
+};
+
+iniciarMenu()      
